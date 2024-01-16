5b3d48e7f59cb0c094674be499886c99cd5fae03
splmloic <l2ct@protonmail.com>
2024-01-16 18:08:38 +0100

    trieur de paires (cetait une GALERE)

--- exo_14.rb
+++ exo_14.rb
@@ -1,23 +1,19 @@
 #création d'un tableau vide
 mailist = []
-#checkeur de Pair (il doit regarder si c'est un multiple de 2)
-def paircheck(mailist)
-    a = mailist.scan(/\d/).first
-        if a.to_i.even? == true
-            puts a
-        end
-end
+
 # création de la liste email
 50.times do |i|
-    if i < 9 #si i inférieur et egal 9 ajoute ça dans l'array
+    if i < 9 #si i inférieur et egal 9 ajoute un 0 pour bien formater dans l'array
         mailist << "jeandupont0#{i + 1}@email.fr"
-    else #Sinon ajoute ça
+    else #Sinon ajoute ça sans 0
         mailist << "jeandupont#{i + 1}@email.fr"
     end
 end
 
-paircheck(mailist)
-# Scan pour savoir qui est pair
-# mailist.scan(/\d/) ça sort le nombre
-# nombre.to_i.even? un booléen qui vrai faux si pair impair
+51.times do |i|
+    if i % 2 == 0
+      puts "jeandupont%02d@email.fr" %[i]
+      mailist.push(i)
+    end
+  end
 
